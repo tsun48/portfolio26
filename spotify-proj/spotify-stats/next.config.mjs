2c5eebcb61bfa2296684ccaa52664e3535392e90
tiffany <tsun48@gmail.com>
2026-04-12 23:19:00 -0400

    config ip

--- spotify-proj/spotify-stats/next.config.mjs
+++ spotify-proj/spotify-stats/next.config.mjs
@@ -4,3 +4,10 @@ const nextConfig = {
 }
 
 export default nextConfig
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  allowedDevOrigins: ['127.0.0.1', '172.20.7.132'],
+}
+
+export default nextConfig
